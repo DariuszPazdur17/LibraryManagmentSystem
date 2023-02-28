7dc501a5b2f8f4096c8877280dc94af8f19d0da3
DariuszPazdur17 <pazdur@student.agh.edu.pl>
2023-02-28 16:57:15 +0100

    Updating readme

--- libraryapp/src/components/Footer/footer.js
+++ libraryapp/src/components/Footer/footer.js
@@ -6,14 +6,13 @@ const Footer = () => {
   return (
     <Box sx={{ background: "#000000", padding: "20px" }}>
       <Typography variant="h6" sx={{ color: "#ffffff" }}>
-        Cecilia Chapman 711-2880 Nulla St. Mankato Mississippi 96522 (257)
-        563-7401
+        Library is open:
       </Typography>
       <Typography variant="h6" sx={{ color: "#ffffff" }}>
         9:00 AM - 7:00 PM
       </Typography>
       <Typography variant="h6" sx={{ color: "#ffffff" }}>
-        xyz@gmail.com
+        libraryB5@gmail.com
       </Typography>
     </Box>
   );
