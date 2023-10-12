028a4bcc4fe90bc8707f4d1d7584a22496a36b4f
Dariusz Pazdur <pazdur.dariusz17@gmail.com>
2023-10-12 17:45:27 +0200

    Update app

--- libraryapp/src/components/News/news.js
+++ libraryapp/src/components/News/news.js
@@ -1,7 +1,7 @@
 import { Typography } from "@mui/material";
 import { Box, Stack } from "@mui/system";
 import React from "react";
-import news from "../../images/news.png";
+import B5 from "../../images/B5.jpg";
 
 const News = () => {
   return (
@@ -13,7 +13,7 @@ const News = () => {
         margin: { lg: "40px 100px" }
       }}
     >
-      <Typography variant="h5">NEWS !!!</Typography>
+      <Typography variant="h3" >Library Of Faculty of Metals Engineering and Industrial Computer Science</Typography>
       <Box
         sx={{
           display: "flex",
@@ -24,16 +24,33 @@ const News = () => {
         }}
       >
         <Box sx={{ maxWidth: { lg: "50%" } }}>
-          <img src={news} />
+          <img src={B5} alt="Library bulding" style={{ width: "20vw", maxWidth: "100%", marginTop: "20px" }} />
         </Box>
         <Box sx={{ maxWidth: { lg: "50%" }, padding: "10px" }}>
           <Typography variant="h6" sx={{ textAlign: "justify" }}>
-           Simply text here
+            <h5>Welcome to Our University Library!</h5>
+
+                At our university library, knowledge knows no bounds. Step into a world where every page turned is a new adventure,
+                where the rustle of paper and the faint hum of ideas intermingle in a symphony of learning. Our library isn't just 
+                a repository of books; it's a sanctuary for curious minds, a haven for passionate explorers, and a hub of intellectual exchange.
+
+            <h5>Discover Limitless Horizons:</h5>
+                Here, you'll find a treasure trove of knowledge spanning countless subjects 
+                - from ancient history to cutting-edge technology, from timeless classics to groundbreaking research. 
+                Our shelves are adorned with books that inspire, educate, and transport you to different worlds. 
+                Whether you're delving into the mysteries of the cosmos, unraveling the threads of history, 
+                or diving deep into the realms of fiction, our library offers a boundless array of literary delights.
+{/* 
+            <h5>A Hub of Creativity:</h5>
+                Beyond the pages, our library pulsates with creativity. It's a place where students huddle over
+                laptops, typing out the next great novel or crafting innovative solutions to real-world problems. 
+                Discussions echo through the air, and collaborative projects come to life. Here, inspiration flows 
+                as freely as the ink on the pages of a novel, and ideas take flight, reaching heights unimagined. */}
           </Typography>
         </Box>
       </Box>
     </Stack>
   );
-};
+}
 
 export default News;
--- libraryapp/src/files/firebase.js
+++ libraryapp/src/files/firebase.js
@@ -3,13 +3,14 @@ import "firebase/compat/firestore";
 import "firebase/compat/auth";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyCIEGkJDK4ki-lp41cxBHULPbZX1sN9CNI",
-  authDomain: "crud-app-react.firebaseapp.com",
-  projectId: "crud-app-react",
-  storageBucket: "crud-app-react.appspot.com",
-  messagingSenderId: "263724024311",
-  appId: "1:263724024311:web:0054eba062de7ba6356ef2",
-  measurementId: "G-59981BVXVH"
+  apiKey: "AIzaSyAQaJGnXaL6GLwfubQ-Qz52vM7LQ1n9fmg",
+  authDomain: "library-managment-5e555.firebaseapp.com",
+  databaseURL: "https://library-managment-5e555-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "library-managment-5e555",
+  storageBucket: "library-managment-5e555.appspot.com",
+  messagingSenderId: "615834683613",
+  appId: "1:615834683613:web:3c4e87db78fa412e3ef334",
+  measurementId: "G-0JDVKN7EHH"
 };
 // Initialize Firebase
 var fire = firebase.initializeApp(firebaseConfig);
